cbfaa28c14479b68271fe0a9145b1fd250579ff3
lakshmanana456 <elakshmanan10501@gmail.com>
2025-06-30 10:50:27 +0530

    deploy error commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AppContextProvider from './context/AppContext.jsx'
-import { HashRouter } from 'react-router-dom'
+import { HashRouter} from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-   <HashRouter>
-    <AppContextProvider>
-      <App/>
-    </AppContextProvider>
-   </HashRouter>
-
-
+createRoot(document.getElementById('root')).render(
+  <HashRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>
+  </HashRouter>
 )
